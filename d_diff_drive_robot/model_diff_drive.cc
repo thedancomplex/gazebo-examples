fcc29f0544c3d6cecde60673899acba23c2a4dab
GMU Student <student@ubuntu.(none)>
2014-03-25 12:51:52 -0400

    fixed ach on server side

--- d_diff_drive_robot/model_diff_drive.cc
+++ d_diff_drive_robot/model_diff_drive.cc
@@ -4,12 +4,39 @@
 #include <gazebo/common/common.hh>
 #include <stdio.h>
 
+
+// For Ach
+#include <errno.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <math.h>
+#include <inttypes.h>
+#include <ach.h>
+#include <string.h>
+
+// ach channels
+ach_channel_t chan_diff_drive_ref;      // hubo-ach
+
+int debug = 0;
+double H_ref[2] = {};
+
 namespace gazebo
 {   
   class ModelDiffDrive : public ModelPlugin
   {
     public: void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf) 
     {
+
+      // Open Ach channel
+        /* open ach channel */
+        memset( &H_ref,   0, sizeof(H_ref));
+        int r = ach_open(&chan_diff_drive_ref, "robot-diff-drive" , NULL);
+        assert( ACH_OK == r );
+        ach_put(&chan_diff_drive_ref, &H_ref , sizeof(H_ref));
 
       // Store the pointer to the model
       this->model = _parent;
@@ -63,12 +90,39 @@ namespace gazebo
     // Called by the world update start event
     public: void OnUpdate()
     {
-      //this->left_wheel_joint_->SetMaxForce(0, 10);
+
+      // Get Ach chan data
+    size_t fs;
+    int r = ach_get( &chan_diff_drive_ref, &H_ref, sizeof(H_ref), &fs, NULL, ACH_O_LAST );
+
+    if(ACH_OK != r | ACH_STALE_FRAMES != r | ACH_MISSED_FRAME != r) {
+                if(debug) {
+                  //      printf("Ref ini r = %s\n",ach_result_to_string(r));}
+                   printf("ref int r = %i \n\r",r);
+		 }
+        }
+        else{   assert( sizeof(H_ref) == fs ); }
+
+
+//    if(ACH_OK != r  || ACH_STALE_FRAMES != r || ACH_MISSED_FRAME != r) {
+//      fprintf(stderr, "Ref r %i ",r);
+//    }
+//    else{
+//      assert( sizeof(H_ref) == fs);
+
+      this->right_wheel_joint_->SetForce(0, H_ref[0]);
+      this->left_wheel_joint_->SetForce(0, H_ref[1]);
+//    }
+
+     //this->left_wheel_joint_->SetMaxForce(0, 10);
       //this->right_wheel_joint_->SetMaxForce(0, 10);
       //this->left_wheel_joint_->SetForce(0, -0.5);
-      this->left_wheel_joint_->SetForce(0, 0.2);
+//      this->left_wheel_joint_->SetForce(0, 0.2);
       //this->right_wheel_joint_->SetVelocity(0,0.5);
-      this->right_wheel_joint_->SetForce(0, -0.2);
+//      this->right_wheel_joint_->SetForce(0, -0.2);
+
+      
+
     }
 
     // Pointer to the model
